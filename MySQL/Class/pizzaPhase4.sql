9f5a67ae3ebb0fae6345625c76b3b163a4c169b3
Sean <spmyers@protonmail.com>
2018-04-07 17:33:04 -0400

    add pizza db query file

---
+++ MySQL/Class/pizzaPhase4.sql
@@ -0,0 +1,12 @@
+--1.)What are the current inventory levels for each topping?
+SELECT Name, Inventory FROM TOPPING
+;
+--2.)What is the name of each customer,how many orders have they placed, and how many pizzas have they ordered?
+--3.)What discounts provide a percentage off, and what is that percentage?
+--4.)What toppings cost the customer less than a dollar toadd to their pizza?
+--5.)For each topping, how much does it cost the business to add that topping toeach size of pizza. Note that the cost is per unit, and there are different amounts of the topping that go on each size.
+--6.)What pizzas are on the order placed byAndrew Wilkes-Krier on March 5th? What are the sizes, crust type, and toppings on those pizzas. Your results should have multiple rows for each pizza (to account for multiple toppings on a pizza), but you should make sure that all of the information for a pizza is on consecutive rows in your result. Include the Pizza ID to make sure that the employee prepping the pizza can tell which topping goes on which pizza.
+--7.)On what days did a customer place an order and use any of the “Lunch Special” discounts?
+--8.)What Customersdo we not have an address on file for?
+--9.)What is the base price for a Large Gluten Free pizza?
+--10.)For each Topping, show how many large pizzas we could make with that topping based on the amount of inventory we currently have.
