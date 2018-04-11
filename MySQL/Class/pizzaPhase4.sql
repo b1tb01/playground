48b8de97c05424aadca2ae6c85581d0a18c18236
Sean <spmyers@protonmail.com>
2018-04-11 18:36:58 -0400

    correcting comments phase 4

--- MySQL/Class/pizzaPhase4.sql
+++ MySQL/Class/pizzaPhase4.sql
@@ -1,10 +1,10 @@
 use PizzaProjectDB_3sh2;
---1.)What are the current inventory levels for each topping?
+/*1.)What are the current inventory levels for each topping? */
 SELECT Name, Inventory 
 FROM TOPPING
 ;
---2.)What is the name of each delivery customer,how many orders have they placed, 
---and how many pizzas have they ordered?
+/*2.)What is the name of each delivery customer,how many orders have they placed, 
+and how many pizzas have they ordered?*/
 SELECT DISTINCT Fname AS First_Name, Lname AS Last_Name, 
 COUNT(DISTINCT ORDERS.O_ID) AS Num_of_Orders, SUM(Num_of_Pizza) AS Num_of_Pizzas_Ordered
 FROM ((CUSTOMER JOIN ORDERS ON CUSTOMER.C_ID = ORDERS.C_ID) 
@@ -12,27 +12,27 @@ JOIN PIZZA ON ORDERS.O_ID=PIZZA.O_ID)
 WHERE Type="Delivery"
 GROUP BY CUSTOMER.C_ID
 ;
---3.)What discounts provide a percentage off, and what is that percentage?
+/*3.)What discounts provide a percentage off, and what is that percentage?*/
 SELECT DISTINCT Name AS Discount_Name, PercentOff*100 AS Percentage_Off
 FROM DISCOUNT
 WHERE PercentOff > 0
 ;
---4.)What toppings cost the customer less than a dollar to add to their pizza?
+/*4.)What toppings cost the customer less than a dollar to add to their pizza?*/
 SELECT Name As Topping_Name, Price
 FROM TOPPING
 WHERE Price < 1.00
 ;
---5.)For each topping, how much does it cost the business to add that topping to each size of pizza. 
---Note that the cost is per unit, and there are different amounts of the topping that go on each size.
+/*5.)For each topping, how much does it cost the business to add that topping to each size of pizza. 
+Note that the cost is per unit, and there are different amounts of the topping that go on each size.*/
 SELECT Name AS Topping_Name, CostPerUnit*Small AS Small_Cost, CostPerUnit*Medium AS Medium_Cost,
 CostPerUnit*Large AS Large_Cost, CostPerUnit*x_Large AS X_Large_Cost
 FROM TOPPING
 ;
---6.)What pizzas are on the order placed by Andrew Wilkes-Krier on March 5th? What are the sizes, 
---crust type, and toppings on those pizzas. Your results should have multiple rows for each pizza 
---(to account for multiple toppings on a pizza), but you should make sure that all of the information 
---for a pizza is on consecutive rows in your result. Include the Pizza ID to make sure that the 
---employee prepping the pizza can tell which topping goes on which pizza.
+/*6.)What pizzas are on the order placed by Andrew Wilkes-Krier on March 5th? What are the sizes, 
+crust type, and toppings on those pizzas. Your results should have multiple rows for each pizza 
+(to account for multiple toppings on a pizza), but you should make sure that all of the information 
+for a pizza is on consecutive rows in your result. Include the Pizza ID to make sure that the 
+employee prepping the pizza can tell which topping goes on which pizza. */
 SELECT PIZZA.P_ID, Size, Crust_Type, TOPPING.Name AS Topping_Name
 FROM ((((PIZZA JOIN TOPPING_PIZZA ON PIZZA.P_ID=TOPPING_PIZZA.P_ID)
 JOIN ORDERS ON PIZZA.O_ID=ORDERS.O_ID) JOIN CUSTOMER ON ORDERS.C_ID=CUSTOMER.C_ID)
@@ -40,13 +40,13 @@ JOIN TOPPING ON TOPPING_PIZZA.T_ID=TOPPING.T_ID)
 WHERE Fname LIKE "%Andrew%" AND Timestamps LIKE "%March 5th%"
 ORDER BY PIZZA.P_ID ASC
 ;
---7.)On what days did a customer place an order and use any of the “Lunch Special” discounts?
+/*7.)On what days did a customer place an order and use any of the “Lunch Special” discounts?*/
 SELECT PIZZA.Timestamps
 FROM (((ORDERS JOIN DISCOUNT_ORDERS ON ORDERS.O_ID=DISCOUNT_ORDERS.O_ID)
 JOIN DISCOUNT ON DISCOUNT_ORDERS.D_ID=DISCOUNT.D_ID) JOIN PIZZA ON ORDERS.O_ID=PIZZA.O_ID)
 WHERE DISCOUNT.Name LIKE "%Lunch Special%"
 ;
---8.)What Customers do we not have an address on file for?
+/*8.)What Customers do we not have an address on file for? */
 SELECT DISTINCT Fname as First_Name, Lname as Last_Name
 FROM ((CUSTOMER LEFT OUTER JOIN ORDERS ON CUSTOMER.C_ID=ORDERS.C_ID) LEFT OUTER JOIN DELIVERY ON ORDERS.O_ID=DELIVERY.O_ID)
 WHERE NOT ORDERS.Type="Delivery" AND CUSTOMER.C_ID NOT IN (
@@ -54,13 +54,13 @@ WHERE NOT ORDERS.Type="Delivery" AND CUSTOMER.C_ID NOT IN (
    FROM ((CUSTOMER JOIN ORDERS ON CUSTOMER.C_ID=ORDERS.C_ID) JOIN DELIVERY ON ORDERS.O_ID=DELIVERY.O_ID)
    WHERE ORDERS.Type="Delivery")
 ;
---9.)What is the base price for a Large Gluten Free pizza?
+/*9.)What is the base price for a Large Gluten Free pizza? */
 SELECT Price AS Base_Price
 FROM BASE_PRICE
 WHERE Size="Large" AND Crust_Type="Gluten-Free"
 ;
---10.)For each Topping, show how many large pizzas we could make with that topping based on the 
---amount of inventory we currently have.
+/*10.)For each Topping, show how many large pizzas we could make with that topping based on the 
+amount of inventory we currently have. */
 SELECT TOPPING.Name AS Topping, FLOOR(Inventory/Large) AS Num_Large_Pizzas
 FROM TOPPING
 ;
